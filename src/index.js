32069785e9060ae24506730aba862fdaa5b0777e
Michaël Zasso <targos@protonmail.com>
2016-05-19 13:01:17 +0200

    pick binarySearch from standalone package

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = exports = require('./ArrayUtils');
 exports.getEquallySpacedData = require('./getEquallySpaced').getEquallySpacedData;
 exports.SNV = require('./snv').SNV;
+exports.binarySearch = require('ml-binary-search');
