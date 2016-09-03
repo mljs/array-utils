a45d3b87b19c8a44d64bbf920b6ced405fc2cc53
lpatiny <gb252243>
2016-09-03 17:27:38 +0200

    Put getClosest and uniqueX in separates packages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ module.exports = exports = require('./ArrayUtils');
 
 exports.getEquallySpacedData = require('./getEquallySpaced').getEquallySpacedData;
 exports.SNV = require('./snv').SNV;
-exports.getClosest = require('./getClosest');
-exports.uniqueX = require('./uniqueX');
 exports.binarySearch = require('ml-binary-search');
 
