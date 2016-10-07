56fbf3ce47e7ba1596b985da31eef047b2fa3ed8
Michaël Zasso <targos@protonmail.com>
2016-10-07 10:39:42 +0200

    feat: remove binarySearch
    The function is available on npm.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,3 @@ module.exports = exports = require('./ArrayUtils');
 
 exports.getEquallySpacedData = require('./getEquallySpaced').getEquallySpacedData;
 exports.SNV = require('./snv').SNV;
-exports.binarySearch = require('ml-binary-search');
-
