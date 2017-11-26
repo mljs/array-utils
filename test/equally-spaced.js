324c846f3835d8157e2caf5a932f2850c14b97b2
Jose Alejandro Bolanos Arroyave <jose.bolanos@correounivalle.edu.co>
2017-11-26 05:53:44 -0500

    add test for smooth variant with particular case

--- test/equally-spaced.js
+++ test/equally-spaced.js
@@ -112,20 +112,6 @@ describe('get equally spaced data', function () {
         ans[0].should.be.equal(2.5);
         ans[1].should.be.equal(10);
 
-        x = [10, 5, 0];
-        y = [10, 5, 0];
-
-        var ans = getEquallySpacedData(x, y, {
-            from: 0,
-            to: 10,
-            numberOfPoints: 2,
-            variant: 'slot'
-        });
-
-        ans[0].should.be.equal(2.5);
-        ans[1].should.be.equal(10);
-
-
         x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         y = [0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0];
         ans = getEquallySpacedData(x, y, {
@@ -150,11 +136,11 @@ describe('get equally spaced data', function () {
             numberOfPoints: 4,
             variant: 'smooth'
         });
-
-        ans[0].should.be.equal(6);
-        ans[1].should.be.equal(5);
-        ans[2].should.be.equal(4);
-        ans[3].should.be.equal(3);
+        
+        ans[3].should.be.equal(6);
+        ans[2].should.be.equal(5);
+        ans[1].should.be.equal(4);
+        ans[0].should.be.equal(3);
     });
 
     describe('on jcamp data', function () {
@@ -197,25 +183,25 @@ describe('get equally spaced data', function () {
             });
             ans.map(x => x.should.equal(0));
         });
-        it.only('Array with negative numbers', () => {
+        it('Array with negative numbers by smooth', () => {
             let data = require('./data/debug.json');
             let {x, y} = data;
-            // let firstX = x[0];
-            // let lastX = x[x.length - 1];
-            // let middle = (firstX + lastX) / 2;
-            // let newX = x.map((e) => e - middle);
-            // let sum = 0;
-            // console.log(newX[0]);
-            // console.log(newX[newX.length - 1]);
+
+            let j = 0;
+            while (x[j] >= 0) j++
+            let index0 = j
+            while (x[j] >= 200) j++
+            let index200 = j;
+
             let ans = getEquallySpacedData(x, y, {
                 from: 0,
                 to: 200,
-                numberOfPoints: 4*1024,
+                numberOfPoints: 7 *1024,
                 variant: 'smooth'
             });
-            let index = x.indexOf(0);
-            console.log(ans.length);
-            console.log(y[3018], ans[0]);
+
+            (y[index0] * 1.1 <= ans[0]).should.be.ok();
+            (y[index200] * 1.1 <= ans[7 * 1024 - 1]).should.be.ok();
         })
     });
 
